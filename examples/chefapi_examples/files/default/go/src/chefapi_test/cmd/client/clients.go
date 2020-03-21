0fe43f0884f833dbcd1f39f9feb6f32225a88cb8
markgibbons <mark.gibbons@nordstrom.com>
2020-03-20 20:16:57 -0700

    Add client, environment and role examples and tests

---
+++ examples/chefapi_examples/files/default/go/src/chefapi_test/cmd/client/clients.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"chefapi_test/testapi"
+	"github.com/go-chef/chef"
+)
+
+func main() {
+	// Use the default test org
+	client := testapi.Client()
+
+	// List initial clients
+	clientList, err := client.Clients.List()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Couldn't list clients: ", err)
+	}
+	fmt.Println("List initial clients", clientList)
+
+	// Define a Client object
+	client1 := chef.NewClient("client1")
+	fmt.Println("Define client1", client1)
+
+	// Delete client1 ignoring errors :)
+	err = client.Clients.Delete(client1.Name)
+
+	// Create
+	clientResult, err := client.Clients.Post(client1)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Couldn't create client client1. ", err)
+	}
+	fmt.Println("Added client1", clientResult)
+
+	// List clients
+	clientList, err = client.Clients.List()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Couldn't list clients: ", err)
+	}
+	fmt.Println("List clients after adding client1", clientList)
+
+	// Create a second time
+	clientResult, err = client.Clients.Post(client1)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Couldn't recreate client client1. ", err)
+	}
+	fmt.Println("Added client1", clientResult)
+
+	// Read client1 information
+	serverClient, err := client.Clients.Get("client1")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Couldn't get client: ", err)
+	}
+	fmt.Printf("Get client1 %+v\n", serverClient)
+
+	// update client
+	// TODO - update something about the client
+	updateClient, err := client.Clients.Put(client1)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Couldn't update client: ", err)
+	}
+	fmt.Println("Update client1", updateClient)
+
+	// Info after update
+	serverClient, err = client.Clients.Get("client1")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Couldn't get client: ", err)
+	}
+	fmt.Printf("Get client1 after update %+v\n", serverClient)
+
+	// Delete client ignoring errors :)
+	err = client.Clients.Delete(client1.Name)
+	fmt.Println("Delete client1", err)
+
+	// List clients
+	clientList, err = client.Clients.List()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Couldn't list clients: ", err)
+	}
+	fmt.Println("List clients after cleanup", clientList)
+}
